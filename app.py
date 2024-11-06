52fb4caf21072cec82977393fb6d2fd040468374
matt-presti <mapresti3@gmail.com>
2024-11-06 19:49:15 +0000

    syntax error fixed in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
-@app.route(/'db_test')
+@app.route('/db_test')
 def testing():
 	conn = psycopg2.connect("postgresql://bball_render_practice_user:zDS09VBqss7InRDeoCxv1fHJpebyIhBf@dpg-cslsa3lumphs73bhvjr0-a/bball_render_practice")
 	conn.close()
