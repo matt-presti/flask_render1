ce31f586b8b759d022c8659799721edfe7254dcf
matt-presti <mapresti3@gmail.com>
2024-11-06 20:01:33 +0000

    added route: db_create

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def testing():
 @app.route('/')
 def hello_world():
     return 'Hello World from Matthew Presti in 3308'
+
+@app.route('/db_create')
+def create_tables():
+	conn = psycopg2.connect("postgresql://bball_render_practice_user:zDS09VBqss7InRDeoCxv1fHJpebyIhBf@dpg-cslsa3lumphs73bhvjr0-a/bball_render_practice")
+	cur = conn.cursor()
+	cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+		''')
+	con.commit()
+	con.close()
+	
